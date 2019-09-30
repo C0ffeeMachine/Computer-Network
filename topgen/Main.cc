8893c3e543a43aeeb267bf2051cb6deac555da79
C0ffeeMachine <43390126+C0ffeeMachine@users.noreply.github.com>
2019-10-01 00:14:38 +0530

    Update Main.cc
    created class to divide tasks to different functions.

--- topgen/Main.cc
+++ topgen/Main.cc
@@ -4,6 +4,7 @@
 #include <vector>
 #include <map>
 #include "MyApp.h"
+
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE("Evaluation-Phase1");
@@ -20,6 +21,214 @@ RxDrop(Ptr<PcapFileWrapper> file, Ptr<const Packet> p) {
 	NS_LOG_UNCOND("RxDrop at " << Simulator::Now().GetSeconds());
 	file->Write(Simulator::Now(), p);
 }*/
+
+class Frag
+{
+	public:
+		int nLN = 10, nRN = 13, nR = 3, nTN = 3;
+
+		NodeContainer leftNodes;	// Left Nodes
+		NodeContainer rightNodes;	// right nodes
+		NodeContainer routerNodes;	// router
+		NodeContainer topNodes;		// topnode
+
+		PointToPointHelper p2p;
+		PointToPointHelper bt1;
+		PointToPointHelper bt2;
+
+		std::map<std::string, NodeContainer> NC;
+		std::map<std::string, NetDeviceContainer> NDC;
+
+		InternetStackHelper internetStack;
+
+		std::map<std::string, Ipv4InterfaceContainer> IC;
+
+		Ipv4AddressHelper ipv4;
+
+		int activeCon = nLN + nTN - 2;
+
+		uint16_t sinkPortRight[activeCon];
+		std::vector<Address> sinkAddRight;
+		std::vector<PacketSinkHelper> packetSinkHelperRight;
+		ApplicationContainer sinkRight[activeCon];
+		Ptr<Socket> socketLeft[activeCon - nTN + 1], socketTop[nTN - 1];
+		Ptr<MyApp> app[activeCon];
+
+	private:
+		void
+		CreateNode()
+		{
+			NS_LOG_INFO("Create nodes.");
+
+			leftNodes.Create(nLN);
+			topNodes.Create(nTN);
+			rightNodes.Create(nRN);
+			routerNodes.Create(nR);
+		}
+		void
+		BuildLink()
+		{
+			p2p.SetDeviceAttribute("DataRate", StringValue("50Mbps"));
+			p2p.SetChannelAttribute("Delay", StringValue("10ms"));
+
+			bt1.SetDeviceAttribute("DataRate", StringValue("20Mbps"));
+			bt1.SetChannelAttribute("Delay", StringValue("20ms"));
+
+			bt2.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
+			bt2.SetChannelAttribute("Delay", StringValue("10ms"));
+		}
+		void
+		NetDevice()
+		{
+			std::ostringstream oss;
+
+			for (int i = 0; i < nLN - 1; i++)//Left nodes
+			{
+				oss << i << "-r0";
+				NC.insert({ oss.str(), NodeContainer(leftNodes.Get(i),routerNodes.Get(0)) });
+				NDC.insert({ oss.str(), p2p.Install(NC[oss.str()]) });
+				oss.str("");
+				oss.clear();
+			}
+
+			for (int i = 0; i < nTN - 1; i++)//Top nodes
+			{
+				oss << i << "-r1";
+				NC.insert({ oss.str(), NodeContainer(topNodes.Get(i),routerNodes.Get(1)) });
+				NDC.insert({ oss.str(), p2p.Install(NC[oss.str()]) });
+				oss.str("");
+				oss.clear();
+			}
+
+			for (int i = 0; i < nRN - 2; i++)//Right nodes
+			{
+				oss << i << "-r" << nR - 1;
+				NC.insert({ oss.str(), NodeContainer(rightNodes.Get(i),routerNodes.Get(nR - 1)) });
+				NDC.insert({ oss.str(), p2p.Install(NC[oss.str()]) });
+				oss.str("");
+				oss.clear();
+			}
+
+			for (int i = 0; i < nR - 1; i++)//Routers
+			{
+				oss << 'r' << i << "-r" << i + 1;
+				NC.insert({ oss.str(), NodeContainer(routerNodes.Get(i),routerNodes.Get(i + 1)) });
+				oss.str("");
+				oss.clear();
+			}
+
+			NDC.insert({ "r0-r1", bt1.Install(NC["r0-r1"]) });	//manually setting bottleneck atrributes to routers
+			NDC.insert({ "r1-r2", bt2.Install(NC["r1-r2"]) });
+		}
+		void
+		InternetStack()
+		{
+			internetStack.Install(leftNodes);
+			internetStack.Install(rightNodes);
+			internetStack.Install(topNodes);
+			internetStack.Install(routerNodes);
+		}
+		void
+		AssignIp()
+		{
+			NS_LOG_INFO("Assign IP Addresses.");
+
+			std::ostringstream oss;
+			std::ostringstream ip;
+
+
+			for(int i=0; i < nLN-1; i++) 
+  			{
+				ip << "10.1." << i + 1 << ".0";
+				oss << i << "-r0";
+				ipv4.SetBase(ip.str().c_str(), "255.255.255.0");
+				IC.insert({ oss.str(), ipv4.Assign(NDC[oss.str()]) });
+				oss.str("");
+				oss.clear();
+				ip.str("");
+				ip.clear();
+			}
+
+			for (int i = 0; i < nTN - 1; i++) 
+  			{
+				ip << "10.3." << i + 1 << ".0";
+				oss << i << "-r1";
+				ipv4.SetBase(ip.str().c_str(), "255.255.255.0");
+				IC.insert({ oss.str(), ipv4.Assign(NDC[oss.str()]) });
+				oss.str("");
+				oss.clear();
+				ip.str("");
+				ip.clear();
+			}
+
+			for (int i = 0; i < nRN - 2; i++) 
+  			{
+				ip << "10.2." << i + 1 << ".0";
+				oss << i << "-r" << nR - 1;
+				ipv4.SetBase(ip.str().c_str(), "255.255.255.0");
+				IC.insert({ oss.str(), ipv4.Assign(NDC[oss.str()]) });
+				oss.str("");
+				oss.clear();
+				ip.str("");
+				ip.clear();
+			}
+
+			for (int i = 0; i < nR - 1; i++)
+ 			{
+				ip << "10.4." << i + 1 << ".0";
+				oss << 'r' << i << "-r" << i + 1;
+				ipv4.SetBase(ip.str().c_str(), "255.255.255.0");
+				IC.insert({ oss.str(), ipv4.Assign(NDC[oss.str()]) });
+				oss.str("");
+				oss.clear();
+				ip.str("");
+				ip.clear();
+			}
+		}
+		void
+		TcpConnection()
+		{
+			NS_LOG_INFO("Create Applications.");
+
+			for (int i = 0; i < activeCon; i++) 
+  			{
+				oss << i << "-r" << nR - 1;
+				sinkPortRight[i] = 8080 + i;
+				sinkAddRight.push_back(Address(InetSocketAddress(IC[oss.str()].GetAddress(0), sinkPortRight[i])));
+				packetSinkHelperRight.push_back(PacketSinkHelper("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), sinkPortRight[i])));
+				sinkRight[i] = packetSinkHelperRight[i].Install(rightNodes.Get(i));
+				sinkRight[i].Start(Seconds(0));
+				sinkRight[i].Stop(Seconds(200.));
+
+				if (i < nLN - 1)
+     			{
+					socketLeft[i] = Socket::CreateSocket(leftNodes.Get(i), TcpSocketFactory::GetTypeId());
+
+					app[i] = CreateObject<MyApp>();
+					app[i]->Setup(socketLeft[i], sinkAddRight[i], 1040, 100000, DataRate("100Mbps"));
+					leftNodes.Get(i)->AddApplication(app[i]);
+					app[i]->SetStartTime(Seconds(1));
+					app[i]->SetStopTime(Seconds(200.));
+				}
+
+    			else 
+    			{
+					socketTop[i - ( nLN - 1 )] = Socket::CreateSocket(topNodes.Get(i - ( nLN - 1 )), TcpSocketFactory::GetTypeId());
+
+					app[i - ( nLN - 1 )] = CreateObject<MyApp>();
+					app[i - ( nLN - 1 )]->Setup(socketTop[i - ( nLN - 1 )], sinkAddRight[i], 1040, 100000, DataRate("100Mbps"));
+					topNodes.Get(i - ( nLN - 1 ))->AddApplication(app[i - ( nLN - 1 )]);
+					app[i - ( nLN - 1 )]->SetStartTime(Seconds(1));
+					app[i - ( nLN - 1 )]->SetStopTime(Seconds(200.));
+				}
+
+				oss.str("");
+				oss.clear();
+			}
+		}
+
+};
+
 int main(int argc, char *argv[]) {
 	std::string probeType = "ns3::Ipv4PacketProbe";
 	std::string tracePath = "/NodeList/*/$ns3::Ipv4L3Protocol/Tx";
@@ -29,7 +238,7 @@ int main(int argc, char *argv[]) {
 	CommandLine cmd;
 	cmd.Parse(argc, argv);
 
-  //
+ /* //
   // Explicitly create the nodes required by the topology (shown above).
   //
 	NS_LOG_INFO("Create nodes.");
@@ -104,7 +313,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	NDC.insert({ "r0-r1", bt1.Install(NC["r0-r1"]) });	//manually setting bottleneck atrributes to routers
-	NDC.insert({ "r1-r2", bt2.Install(NC["r1-r2"]) });
+	NDC.insert({ "r1-r2", bt2.Install(NC["r1-r2"]) });*/
 
   //
   // Error model for congestion window
@@ -114,7 +323,7 @@ int main(int argc, char *argv[]) {
 	em->SetAttribute("ErrorRate", DoubleValue(0.00001));
 	NDC["0-r2"].Get(0)->SetAttribute("ReceiveErrorModel", PointerValue(em));
 
-  //
+ /* //
   // Install Internet Stack
   //
 	InternetStackHelper internetStack;
@@ -179,7 +388,7 @@ int main(int argc, char *argv[]) {
 		oss.clear();
 		ip.str("");
 		ip.clear();
-	}
+	}*/
 
   //
   // Turn on global static routing so we can actually be routed across the network.
@@ -188,7 +397,7 @@ int main(int argc, char *argv[]) {
 	NS_LOG_INFO("Enable static global routing.");
 	Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
-  //
+ /* //
   //Create TCP and UDP applications.
   //
 
@@ -240,7 +449,7 @@ int main(int argc, char *argv[]) {
 
 		oss.str("");
 		oss.clear();
-	}
+	}*/
 
   //---------------------------------------------NetAnim---------------------------------------------------------------
 	
